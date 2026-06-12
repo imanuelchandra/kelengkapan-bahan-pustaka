3df0a2431bea30cc878f6b089cc3caa1a162f4de
unknown <lefta.chandra@gmail.com>
2026-06-12 21:53:13 +0900

    update kelengkapan

--- kelengkapan_bahan_pustaka.plugin.php
+++ kelengkapan_bahan_pustaka.plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Pemustaka Untuk SLiMS
+ * Plugin Name: Kelengkapan Bahan Pustaka Untuk SLiMS
  * Plugin URI: -
- * Description: Plugins pemustaka dan laporan pemustaka untuk SLIMS
+ * Description: Plugins kelengkapan bahan pustaka dan laporan kelengkapan bahan pustaka untuk SLIMS
  * Version: 1.0.0
  * Author: -
  * Author URI: -
@@ -20,7 +20,7 @@ $pathStatistikKelengkapanBahanPustaka = __DIR__ . '/pages/statistik_kelengkapan_
 //$path =  __DIR__ . '/pages/inventarisasi.php';
  // Make default group menu
 
-Plugins::group('Pemustaka', function() use($pathKelengkapanBahanPustaka, $pathRekapitulasiKelengkapanBahanPustaka) {
+Plugins::group('Kelengkapan Bahan Pustaka', function() use($pathKelengkapanBahanPustaka, $pathRekapitulasiKelengkapanBahanPustaka) {
             // Scan all file inside module directory as menu
  Plugins::menu('reporting', 'Daftar Kelengkapan Bahan Pustaka', $pathKelengkapanBahanPustaka);
  Plugins::menu('reporting', 'Rekapitulasi Kelengkapan Bahan Pustaka', $pathRekapitulasiKelengkapanBahanPustaka);
@@ -28,7 +28,7 @@ Plugins::group('Pemustaka', function() use($pathKelengkapanBahanPustaka, $pathRe
 });
 
 
-// Plugins::group('Kelengkapan Bahan Pustaka', function() use($pathStatistikKelengkapanBahanPustaka) {
-//             // Scan all file inside module directory as menu
-//   Plugins::menu('reporting', 'Statistik Kelengkapan Bahan Pustaka', $pathStatistikKelengkapanBahanPustaka);
-// });
+Plugins::group('Kelengkapan Bahan Pustaka', function() use($pathStatistikKelengkapanBahanPustaka) {
+            // Scan all file inside module directory as menu
+  Plugins::menu('reporting', 'Statistik Kelengkapan Bahan Pustaka', $pathStatistikKelengkapanBahanPustaka);
+});
